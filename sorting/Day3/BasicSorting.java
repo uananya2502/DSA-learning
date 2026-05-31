776524fa680b497c17067e119439dacfb6a08fb3
Ananya Upadhyay <uananya2502@gmail.com>
2026-05-31 13:13:26 +0530

    Add sorting algorithms implementation

---
+++ sorting/Day3/BasicSorting.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+public class BasicSorting{
+    public void bubbleSort(int[] arr){
+        for(int turn =0; turn <=arr.length-1; turn++){
+            boolean swap=false;
+            for(int j=0; j<arr.length-1-turn; j++){
+                if(arr[j]<arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1]= temp;
+                    swap=true;
+                }
+            }
+            if(!swap)
+                break;
+        }
+        return arr;
+    }
+
+    public void selectionSort(int[] arr){
+        for(int i =0; i<arr.length-1; i++){
+            int minp = i;
+            for(int j=i+1; j<arr.length; j++){
+                if(arr[minp]< arr[j])
+                    minp =j;
+            }
+            int temp = arr[minp];
+            arr[minp]= arr[i];
+            arr[i]= temp;
+        }
+        return arr;
+    }
+
+    public void insertionSort(int[] arr){
+        for(int i=1; i<arr.length; i++){
+            int curr = arr[i];
+            int prev = i-1;
+            while(prev>=0 && arr[prev]> curr){
+                arr[prev+1]=arr[prev];
+                prev--;
+            }
+            arr[prev+1]=curr;
+        }
+        return arr;
+    }
+    
+    public static void main(String [] args){
+        int[] a = [5,3,4,2,1];
+        int[] b = [5,3,4,2,1];
+        int[] c = [5,3,4,2,1];
+        bubbleSort(a);
+        selectionSort(b);
+        insertionSort(c)
+
+    }
+}
