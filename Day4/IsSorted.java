7204f1a3fec00831376dc38679ae312e0317b900
Ananya Upadhyay <uananya2502@gmail.com>
2026-06-01 22:59:17 +0530

    2 strivers array question Check if the Array is Sorted II and Second largest element

---
+++ Day4/IsSorted.java
@@ -0,0 +1,18 @@
+package Day4;
+
+import java.util.ArrayList;
+
+/*
+    I compare each element with the next one. If any element is larger 
+    than its successor, the array isn't sorted. Otherwise it is sorted. Time O(n), Space O(1).
+*/
+public class IsSorted {
+    public boolean isSorted(ArrayList<Integer> nums) {
+        //your code goes here
+        for(int i =0; i < nums.size()-1; i++){
+            if(nums.get(i) > nums.get(i+1))
+                return false;
+        }
+        return true;
+    }
+}
