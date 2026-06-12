e7d271927cc78913711ecb5213965dc22900767d
Ananya Upadhyay <uananya2502@gmail.com>
2026-06-12 23:53:16 +0530

    Set matrix zero

---
+++ Day15-25/setZeroes.java
@@ -0,0 +1,47 @@
+// package Day15_25;
+
+public class setZeroes {
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        boolean firstColZero = false;
+
+        // Step 1: Use first row and first column as markers
+        for (int i = 0; i < m; i++) {
+            if (matrix[i][0] == 0) {
+                firstColZero = true;
+            }
+
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0; // mark row
+                    matrix[0][j] = 0; // mark column
+                }
+            }
+        }
+
+        // Step 2: Set cells to zero using markers
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        // Step 3: Handle the first row
+        if (matrix[0][0] == 0) {
+            for (int j = 0; j < n; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        // Step 4: Handle the first column
+        if (firstColZero) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+}
